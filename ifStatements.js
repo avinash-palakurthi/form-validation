06af1b50d6d2daf898a42bd633322b94f25a311f
Avinash <palakurthiavinash07@gmail.com>
2023-07-28 22:59:14 +0530

    javascript part completed

---
+++ ifStatements.js
@@ -0,0 +1,29 @@
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, "Username is required");
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === "") {
+    showError(email, "Email is required");
+  } else if (!isValidEmail(email.value)) {
+    showError(email, "Email is not valid");
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === "") {
+    showError(password, "Password is required");
+  } else {
+    showSuccess(password);
+  }
+
+  if (password2.value === "") {
+    showError(password2, " password is required ");
+  } else {
+    showSuccess(password2);
+  }
+});
